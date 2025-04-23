f0ad9f7b6a850d7820208d0b13fabf0394379339
IgnacioPeralta00 <00196321@uca.edu.sv>
2025-04-23 11:39:37 -0600

    feat: eliminarProducto in Carrito

--- src/com/abc/Carrito.java
+++ src/com/abc/Carrito.java
@@ -1,4 +1,5 @@
 package com.abc;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,20 +15,46 @@ public class Carrito {
     public List<Producto> getProductos() {
         return this.productos;
     }
+
     public void setProductos(List<Producto> productos) {
         this.productos = productos;
     }
+
     public int getCantidadProductos() {
         return this.cantidadProductos;
     }
+
     public void setCantidadProductos(int cantidadProductos) {
         this.cantidadProductos = cantidadProductos;
     }
+
     public double getTotal() {
         return this.total;
     }
+
     public void setTotal(double total) {
         this.total = total;
     }
 
+    public void agregarProducto(Producto producto) {
+        this.getProductos().add(producto);
+        System.out.println("'" + producto.getNombre() + "' agregado al carrito");
+    }
+
+    // Eliminar producto por su ID
+    public void eliminarProducto(int ID) {
+        boolean eliminado = false;
+        for (int i = 0; i < this.getProductos().size(); i++) {
+            if (this.getProductos().get(i).getId() == ID) {
+                String nombreProducto = this.getProductos().get(i).getNombre();
+                this.getProductos().remove(i);
+                System.out.println("'" + nombreProducto + "' eliminado al carrito");
+                eliminado = true;
+                break;
+            }
+        }
+        if (!eliminado) {
+            System.out.println("No se puede eliminar el producto");
+        }
+    }
 }
