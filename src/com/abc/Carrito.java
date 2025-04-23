27949429e1edc74c1665f39ecce1b1c6aa3f58ae
IgnacioPeralta00 <00196321@uca.edu.sv>
2025-04-22 22:29:15 -0600

    feat: setters & getter in Carrito class

--- src/com/abc/Carrito.java
+++ src/com/abc/Carrito.java
@@ -1,4 +1,33 @@
 package com.abc;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Carrito {
+    private List<Producto> productos;
+    private int cantidadProductos;
+    private double total;
+
+    public Carrito() {
+        productos = new ArrayList<>();
+    }
+
+    public List<Producto> getProductos() {
+        return this.productos;
+    }
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+    public int getCantidadProductos() {
+        return this.cantidadProductos;
+    }
+    public void setCantidadProductos(int cantidadProductos) {
+        this.cantidadProductos = cantidadProductos;
+    }
+    public double getTotal() {
+        return this.total;
+    }
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
 }
